e23cbb79d103113f9346e88a861259a4d594b092
Jeanniel <jeanniel.zelaznog@gmail.com>
2025-12-22 01:53:24 -0400

    version 1.1

--- api/main.py
+++ api/main.py
@@ -4,33 +4,32 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Optional
 
-program = FastAPI()
+app = FastAPI()
 
 class Anime(BaseModel):
     nombre_anime: str
     selectEP: Optional[str] = None
 
-program.add_middleware(
+app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@program.post("/BuscarAnime")
+@app.post("/BuscarAnime")
 async def anime(anime: Anime):
     animeAPP = appAnime2.Anime()
     return {"listado": animeAPP.listado_anime(anime.nombre_anime)}
 
-@program.post("/SeleccionAnime")
+@app.post("/SeleccionAnime")
 async def anime(anime: Anime):
     animeAPP = appAnime2.Anime()
     return {"Informacion": animeAPP.select_anime(anime.nombre_anime)}
 
-@program.post("/SeleccionCap")
+@app.post("/SeleccionCap")
 async def anime(anime: Anime):
     animeAPP = appAnime2.Anime()
     return {"Url": animeAPP.select_cap(anime.nombre_anime, anime.selectEP)}
 
-app = program
 
