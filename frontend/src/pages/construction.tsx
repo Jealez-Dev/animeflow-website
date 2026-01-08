48166902751c742d4eb6e50f28c1f8b06437cdbf
Jeanniel <jeanniel.zelaznog@gmail.com>
2026-01-08 14:31:49 -0400

    ajustes previos

--- frontend/src/pages/construction.tsx
+++ frontend/src/pages/construction.tsx
@@ -1,7 +1,7 @@
 function construction() {
     return (
         <div className="construction">
-            <img src="/src/assets/construction.png" alt="Pagina en construcción" />
+            <img src="../../src/assets/construction.png" alt="Pagina en construcción" />
             <h1>Pagina en construcción</h1>
             <p>Por favor, vuelve pronto</p>
         </div>
